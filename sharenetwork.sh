788256ac4dd6a43562ed27baa6608b52ee1c7cd1
John Joe <zzhong@hust.edu.cn>
2017-06-12 11:02:25 +0800

    Delete duplicated file.

--- sharenetwork.sh
+++
@@ -1,4 +0,0 @@
-route add default gw 192.168.7.1
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 223.6.6.6" >> /etc/resolv.conf
-echo "nameserver 114.114.114.114" >> /etc/resolv.conf
